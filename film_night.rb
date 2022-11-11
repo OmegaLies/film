13704a9925c4b36549554af1cf2ce5695f8e651f
OmegaLies <omegaunderwall@gmail.com>
2022-11-11 20:08:53 +0300

    from_url -> from_wiki, URL as const in FilmCollection

--- film_night.rb
+++ film_night.rb
@@ -1,16 +1,13 @@
-require "open-uri"
-require "nokogiri"
 require_relative "lib/film"
 require_relative "lib/film_collection"
 
-URL = "https://ru.wikipedia.org/wiki/250_%D0%BB%D1%83%D1%87%D1%88%D0%B8%D1%85_%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B2_%D0%BF%D0%BE_%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B8_IMDb"
-films = FilmCollection.from_url(URL)
+films = FilmCollection.from_wiki
 
 puts <<~GREETENGS
-Вас приветствует программа «Фильм на вечер»!
-Фильм какого режисера вы хотите сегодня посмотреть?
+  Вас приветствует программа «Фильм на вечер»!
+  Фильм какого режисера вы хотите сегодня посмотреть?
 
-Список режисеров:
+  Список режисеров:
 GREETENGS
 
 films.directors.each.with_index(1) { |director, index| puts "#{index}. #{director}" }
--- lib/film_collection.rb
+++ lib/film_collection.rb
@@ -1,6 +1,11 @@
+require "open-uri"
+require "nokogiri"
+
 class FilmCollection
-  def initialize(args)
-    @films = args[:films]
+  URL = "https://ru.wikipedia.org/wiki/250_%D0%BB%D1%83%D1%87%D1%88%D0%B8%D1%85_%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B2_%D0%BF%D0%BE_%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B8_IMDb"
+
+  def initialize(films)
+    @films = films
   end
 
   def directors
@@ -11,20 +16,17 @@ class FilmCollection
     @films.select { |film| film.director == director }.sample
   end
 
-  def self.from_url(url)
-    doc = Nokogiri::HTML.parse(URI.open(url))
-    films = doc.css("tbody>tr").map do |node|
+  def self.from_wiki
+    doc = Nokogiri::HTML.parse(URI.open(URL))
+    films = doc.css("tbody>tr").filter_map do |node|
       lines = node.css(">td>a").map(&:text)
-      if lines.empty?
-        nil
-      else
-        Film.new(
-          title: lines[0],
-          year: lines[1],
-          director: lines[2]
-        )
-      end
+      next if lines.empty?
+      Film.new(
+        title: lines[0],
+        year: lines[1],
+        director: lines[2]
+      )
     end
-    new(films: films.compact)
+    new(films)
   end
 end
